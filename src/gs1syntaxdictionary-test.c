d0d347affc43035461fad3d3cea4ffe5880fb441
Terry Burton <tez@terryburton.co.uk>
2024-07-12 17:33:40 +0100

    Sort test function declarations and test list

--- src/gs1syntaxdictionary-test.c
+++ src/gs1syntaxdictionary-test.c
@@ -37,43 +37,43 @@
 #endif
 
 
-void test_lint_cset82(void);
+void test_lint_couponcode(void);
+void test_lint_couponposoffer(void);
 void test_lint_cset39(void);
 void test_lint_cset64(void);
+void test_lint_cset82(void);
 void test_lint_csetnumeric(void);
 void test_lint_csum(void);
 void test_lint_csumalpha(void);
-void test_lint_key(void);
+void test_lint_hasnondigit(void);
+void test_lint_hh(void);
+void test_lint_hyphen(void);
+void test_lint_hhmm(void);
+void test_lint_iban(void);
 void test_lint_importeridx(void);
-void test_lint_nonzero(void);
-void test_lint_nozeroprefix(void);
-void test_lint_zero(void);
-void test_lint_yesno(void);
-void test_lint_winding(void);
 void test_lint_iso3166(void);
 void test_lint_iso3166999(void);
 void test_lint_iso3166alpha2(void);
 void test_lint_iso4217(void);
-void test_lint_iban(void);
-void test_lint_yyyymmd0(void);
-void test_lint_yyyymmdd(void);
-void test_lint_yymmd0(void);
-void test_lint_yymmdd(void);
-void test_lint_hhmm(void);
-void test_lint_hh(void);
-void test_lint_mm(void);
-void test_lint_ss(void);
-void test_lint_pieceoftotal(void);
-void test_lint_posinseqslash(void);
-void test_lint_pcenc(void);
-void test_lint_couponcode(void);
-void test_lint_couponposoffer(void);
+void test_lint_iso5218(void);
+void test_lint_key(void);
 void test_lint_latitude(void);
 void test_lint_longitude(void);
 void test_lint_mediatype(void);
-void test_lint_hyphen(void);
-void test_lint_iso5218(void);
-void test_lint_hasnondigit(void);
+void test_lint_mm(void);
+void test_lint_nonzero(void);
+void test_lint_nozeroprefix(void);
+void test_lint_pcenc(void);
+void test_lint_pieceoftotal(void);
+void test_lint_posinseqslash(void);
+void test_lint_ss(void);
+void test_lint_winding(void);
+void test_lint_yesno(void);
+void test_lint_yymmd0(void);
+void test_lint_yymmdd(void);
+void test_lint_yyyymmd0(void);
+void test_lint_yyyymmdd(void);
+void test_lint_zero(void);
 
 void test_name_function_map_is_sorted(void);
 void test_gs1_linter_from_name(void);
@@ -81,43 +81,43 @@ void test_gs1_linter_from_name(void);
 
 TEST_LIST = {
 
-	{ "lint_csetnumeric", test_lint_csetnumeric },
-	{ "lint_cset82", test_lint_cset82 },
+	{ "lint_couponcode", test_lint_couponcode },
+	{ "lint_couponposoffer", test_lint_couponposoffer },
 	{ "lint_cset39", test_lint_cset39 },
 	{ "lint_cset64", test_lint_cset64 },
+	{ "lint_cset82", test_lint_cset82 },
+	{ "lint_csetnumeric", test_lint_csetnumeric },
 	{ "lint_csum", test_lint_csum },
 	{ "lint_csumalpha", test_lint_csumalpha },
 	{ "lint_key", test_lint_key },
+	{ "lint_hasnondigit", test_lint_hasnondigit },
+	{ "lint_hh", test_lint_hh },
+	{ "lint_hhmm", test_lint_hhmm },
+	{ "lint_hyphen", test_lint_hyphen },
 	{ "lint_importeridx", test_lint_importeridx },
 	{ "lint_nonzero", test_lint_nonzero },
 	{ "lint_nozeroprefix", test_lint_nozeroprefix },
-	{ "lint_zero", test_lint_zero },
-	{ "lint_yesno", test_lint_yesno },
-	{ "lint_winding", test_lint_winding },
+	{ "lint_iban", test_lint_iban },
 	{ "lint_iso3166", test_lint_iso3166 },
 	{ "lint_iso3166999", test_lint_iso3166999 },
 	{ "lint_iso3166alpha2", test_lint_iso3166alpha2 },
 	{ "lint_iso4217", test_lint_iso4217 },
-	{ "lint_iban", test_lint_iban },
-	{ "lint_yyyymmd0", test_lint_yyyymmd0 },
-	{ "lint_yyyymmdd", test_lint_yyyymmdd },
-	{ "lint_yymmd0", test_lint_yymmd0 },
-	{ "lint_yymmdd", test_lint_yymmdd },
-	{ "lint_hhmm", test_lint_hhmm },
-	{ "lint_hh", test_lint_hh },
-	{ "lint_mm", test_lint_mm },
-	{ "lint_ss", test_lint_ss },
-	{ "lint_pieceoftotal", test_lint_pieceoftotal },
-	{ "lint_posinseqslash", test_lint_posinseqslash },
-	{ "lint_pcenc", test_lint_pcenc },
-	{ "lint_couponcode", test_lint_couponcode },
-	{ "lint_couponposoffer", test_lint_couponposoffer },
+	{ "lint_iso5218", test_lint_iso5218 },
 	{ "lint_latitude", test_lint_latitude },
 	{ "lint_longitude", test_lint_longitude },
 	{ "lint_mediatype", test_lint_mediatype },
-	{ "lint_hyphen", test_lint_hyphen },
-	{ "lint_iso5218", test_lint_iso5218 },
-	{ "lint_hasnondigit", test_lint_hasnondigit },
+	{ "lint_mm", test_lint_mm },
+	{ "lint_pcenc", test_lint_pcenc },
+	{ "lint_pieceoftotal", test_lint_pieceoftotal },
+	{ "lint_posinseqslash", test_lint_posinseqslash },
+	{ "lint_ss", test_lint_ss },
+	{ "lint_winding", test_lint_winding },
+	{ "lint_yesno", test_lint_yesno },
+	{ "lint_yymmd0", test_lint_yymmd0 },
+	{ "lint_yymmdd", test_lint_yymmdd },
+	{ "lint_yyyymmd0", test_lint_yyyymmd0 },
+	{ "lint_yyyymmdd", test_lint_yyyymmdd },
+	{ "lint_zero", test_lint_zero },
 
 	{ "name_function_map_is_sorted", test_name_function_map_is_sorted },
 	{ "gs1_linter_from_name", test_gs1_linter_from_name },
